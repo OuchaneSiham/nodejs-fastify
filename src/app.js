18e60146761bc82ed3f744c3ec24dc7514394c0d
siham ouchane <ouchanesiham42@gmail.com>
2025-12-01 15:24:31 +0100

    routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
-const fastify = require('fastify')({ logger: true });
 const path = require("path");
-const mongoose = require('mongoose');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
+const mongoose = require('mongoose');
+console.log('__dirname:', __dirname);
+const fastify = require('fastify')({ logger: true });
+const userRoutes = require("./routes/user.routes");
+const projectRoutes = require("./routes/project.routes");
+// console.log(process.env.DB_URL);
 mongoose.connect(process.env.DB_URL)
         .then(() => console.log("connected to database"))
         .catch(error => console.log("error", error));
-const userRoutes = require("./routes/user.routes");
-
+        
 fastify.register(userRoutes, {prefix: "/api/v1/users"});
+fastify.register(projectRoutes, {prefix: "/api/v1/projects"});
+
 const start = async() => 
 {
     try{
